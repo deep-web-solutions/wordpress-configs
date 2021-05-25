dc53cfee6883888d387e919c51c129a2a6fa47d4
Antonius Hegyes <a.hegyes@deep-web-solutions.de>
2021-05-25 13:45:29 +0200

    Undid PHP-DI breaking change

--- src/rector/downgrade-php.php
+++ src/rector/downgrade-php.php
@@ -9,9 +9,8 @@ return static function ( ContainerConfigurator $container_configurator ): void {
 	$parameters = $container_configurator->parameters();
 
 	// Set downgrading rules
-	foreach( array( DowngradeSetList::PHP_80, DowngradeSetList::PHP_74, DowngradeSetList::PHP_73, DowngradeSetList::PHP_72, DowngradeSetList::PHP_71, DowngradeSetList::PHP_70, DowngradeSetList::PHP_53 ) as $set ) {
-		$container_configurator->import( $set );
-	}
+	$parameters->set( Option::SETS, array( DowngradeSetList::PHP_80, DowngradeSetList::PHP_74, DowngradeSetList::PHP_73,
+		DowngradeSetList::PHP_72, DowngradeSetList::PHP_71, DowngradeSetList::PHP_70, DowngradeSetList::PHP_53 ) );
 
 	// Set the paths to refactor.
 	$parameters->set( Option::PATHS, json_decode( getenv( 'dws_autloadedFiles' ), true ) );
