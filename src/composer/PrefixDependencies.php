5b3d31077d2901b443dffbab6f5cb9463ccd09e2
Antonius Hegyes <a.hegyes@deep-web-solutions.com>
2021-12-22 15:04:58 +0100

    improved handling of missing dependencies files/directories

--- src/composer/PrefixDependencies.php
+++ src/composer/PrefixDependencies.php
@@ -14,6 +14,43 @@ use Composer\Script\Event;
  */
 class PrefixDependencies {
 	/**
+	 * Action for before installing or updating the packages.
+	 *
+	 * @since   1.0.0
+	 * @version 1.0.0
+	 *
+	 * @param   Event   $event
+	 */
+	public static function preCommand( Event $event ): void {
+		$console_IO = $event->getIO();
+		$vendorDir  = $event->getComposer()->getConfig()->get( 'vendor-dir' );
+
+		$console_IO->write( 'Setting vendor dir as an environment variable...' );
+		putenv( "dws_vendorDir={$vendorDir}" );
+
+		$console_IO->write( 'Making sure autoloaded dependencies files exist...' );
+
+		$composer_package = json_decode( file_get_contents( dirname( $vendorDir ) . '/composer.json' ), true );
+
+		$autoload_files = array_merge( $composer_package['autoload']['files'] ?? array(), $event->isDevMode() ? ( $composer_package['autoload-dev']['files'] ?? array() ) : array() );
+		foreach ( $autoload_files as $file ) {
+			$file = dirname( $vendorDir ) . DIRECTORY_SEPARATOR . $file;
+			if ( ! is_file( $file ) ) {
+				mkdir( dirname( $file ), 0755, true );
+				touch( $file );
+			}
+		}
+
+		$autoload_directories = array_merge( $composer_package['autoload']['classmap'] ?? array(), $event->isDevMode() ? ( $composer_package['autoload-dev']['classmap'] ?? array() ) : array() );
+		foreach ( $autoload_directories as $directory ) {
+			$directory = dirname( $vendorDir ) . DIRECTORY_SEPARATOR . $directory;
+			if ( ! is_dir( $directory ) ) {
+				mkdir( $directory, 0755, true );
+			}
+		}
+	}
+
+	/**
 	 * Action for the 'post-autoload-dump' event.
 	 *
 	 * @since   1.0.0
@@ -34,20 +71,7 @@ class PrefixDependencies {
 			return;
 		}
 
-		$console_IO->write( 'Setting vendor dir as an environment variable...' );
-		putenv( "dws_vendorDir={$vendorDir}" );
-
-		$console_IO->write( 'Making sure autoloaded files exist...' );
-
 		$composer_package = json_decode( file_get_contents( dirname( $vendorDir ) . '/composer.json' ), true );
-		$autoload_files   = array_merge( $composer_package['autoload']['files'] ?? array(), $composer_package['autoload-dev']['files'] ?? array() );
-		foreach ( $autoload_files as $file ) {
-			$file = dirname( $vendorDir ) . DIRECTORY_SEPARATOR . $file;
-			if ( ! is_file( $file ) ) {
-				mkdir( dirname( $file ), 0755, true );
-				touch( $file );
-			}
-		}
 
 		$console_IO->write( 'Setting package name as an environment variable...' );
 		putenv( "dws_packageName={$composer_package['name']}" );
