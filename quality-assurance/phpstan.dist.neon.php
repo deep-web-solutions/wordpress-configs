224063cb4c7336e3f39eebef67b476b076ec4424
Tony Hegyes <a.hegyes@deep-web-solutions.com>
2024-11-29 18:20:58 +0100

    Dynamically handle PHPStan paths

---
+++ quality-assurance/phpstan.dist.neon.php
@@ -0,0 +1,23 @@
+<?php declare( strict_types = 1 );
+
+$config = array();
+$workingDirectory = getcwd();
+
+foreach ( array( 'dependencies', 'vendor/johnpbloch/wordpress-core' ) as $discoverDirectory ) {
+	if ( is_dir( $workingDirectory . '/' . $discoverDirectory ) ) {
+		$config['parameters']['scanDirectories'][] = $workingDirectory . '/' . $discoverDirectory;
+	}
+}
+
+foreach ( array( 'src', 'includes', 'tests' ) as $analyzeDirectory ) {
+	if ( is_dir( $workingDirectory . '/' . $analyzeDirectory ) ) {
+		$config['parameters']['paths'][] = $workingDirectory . '/' . $analyzeDirectory;
+	}
+}
+foreach ( array( 'bootstrap.php', 'functions.php' ) as $analyzeFile ) {
+	if ( is_file( $workingDirectory . '/' . $analyzeFile ) ) {
+		$config['parameters']['paths'][] = $workingDirectory . '/' . $analyzeFile;
+	}
+}
+
+return $config;
